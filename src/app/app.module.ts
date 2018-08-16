b452b373e1e919ecaf3469ae057bee4a00309967
Faran Shaikh <faran4engg@gmail.com>
2018-08-16 12:07:44 +0530

    removed Pipe - extra noise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 // import { ContactDataService } from './services/data.service';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  MatButtonModule, MatCardModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule, MatRadioModule, MatSnackBarModule, MatToolbarModule, MatTooltipModule
-} from '@angular/material';
+import { MatButtonModule, MatCardModule, MatDialogModule, MatFormFieldModule, MatIconModule, MatInputModule, MatRadioModule, MatSnackBarModule, MatToolbarModule, MatTooltipModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxMaskModule } from 'ngx-mask';
@@ -11,15 +9,13 @@ import { AppComponent } from './app.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ContactComponent } from './contact/contact.component';
 import { ContactDataService } from './services/contact.data.service';
-import { FilterContactsPipe } from './filter-contacts.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    ContactFormComponent,
-    FilterContactsPipe
+    ContactFormComponent
   ],
   entryComponents: [ContactFormComponent],
   imports: [
