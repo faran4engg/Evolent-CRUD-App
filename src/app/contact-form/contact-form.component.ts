41e025dfad8f147759c07df305b98b568446931f
Faran Shaikh <faran4engg@gmail.com>
2018-08-19 13:52:56 +0530

    Contact Form Comments

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -12,14 +12,22 @@ import { ContactFormDialog } from './../models/contact-form-dialog';
     styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-    contactForm: FormGroup;
-    submitted: boolean;
-    isEditMode: boolean;
-    formtitle = 'Create Contact';
+
+    contactForm: FormGroup;         // Form data
+    isEditMode: boolean;            // flag to open modal in either create mode or update mode
+    formtitle = 'Create Contact';   // default form title
 
     // convenience of getter for easy access to form fields
     get f() { return this.contactForm.controls; }
 
+    /**
+     *Creates an instance of ContactFormComponent.
+     * @param {FormBuilder} fb
+     * @param {MatDialogRef<ContactFormComponent>} dialogRef
+     * @param {ContactFormDialog} data
+     * @param {ContactDataService} contactServ
+     * @param {StorageService} storageServ
+     */
     constructor(
         private fb: FormBuilder,
         public dialogRef: MatDialogRef<ContactFormComponent>,
@@ -27,19 +35,11 @@ export class ContactFormComponent implements OnInit {
         private contactServ: ContactDataService,
         public storageServ: StorageService) { }
 
-    setval(data: ContactFormDialog) {
-
-        this.contactForm.controls.firstName.setValue(data.firstName);
-        this.contactForm.controls.lastName.setValue(data.lastName);
-        this.contactForm.controls.email.setValue(data.email);
-        this.contactForm.controls.phoneNumber.setValue(data.phoneNumber);
-        this.contactForm.controls.status.setValue(data.status);
-        this.contactForm.controls.id.setValue(data.id);
-
-    }
-
+    /**
+     *Initializing Form fields along with Validators
+     *Also, Sets form value in case its Edit Mode
+     */
     ngOnInit() {
-
         this.contactForm = this.fb.group({
             firstName: ['', Validators.required],
             lastName: ['', Validators.required],
@@ -49,6 +49,7 @@ export class ContactFormComponent implements OnInit {
             id: ['']
         });
 
+        //  if we have data: <ContactFormDialog> then we are sure that modal will be opening in Edit Mode
         if (this.data) {
             this.setval(this.data);
             this.isEditMode = true;
@@ -56,8 +57,13 @@ export class ContactFormComponent implements OnInit {
         }
     }
 
+    /**
+     *Form Submit
+     *Resusing Storage Service's snackbar feature
+     *Reusing Contact Service's Add / Edit method
+     * @param {ContactFormDialog} contactFormData
+     */
     onSubmit(contactFormData: ContactFormDialog) {
-
 
         if (this.isEditMode) {
             this.contactServ.edit(contactFormData);
@@ -66,8 +72,24 @@ export class ContactFormComponent implements OnInit {
             this.contactServ.add(contactFormData);
             this.storageServ.showSnackBar('Contact Added Successfully', 'OK');
         }
-
+        // Closing Dialog Box
         this.dialogRef.close();
+    }
+
+    /**
+     *Sets Form values in edit mode
+     * @private
+     * @param {ContactFormDialog} data
+     */
+    private setval(data: ContactFormDialog) {
+
+        this.contactForm.controls.firstName.setValue(data.firstName);
+        this.contactForm.controls.lastName.setValue(data.lastName);
+        this.contactForm.controls.email.setValue(data.email);
+        this.contactForm.controls.phoneNumber.setValue(data.phoneNumber);
+        this.contactForm.controls.status.setValue(data.status);
+        this.contactForm.controls.id.setValue(data.id);
+
     }
 
 
